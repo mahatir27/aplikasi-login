ccf14dbd155a515a68b017653c02fd1156270f3d
Mahatir Muhammad <mahatirm44@gmail.com>
2025-02-24 02:03:19 +0800

    Update login-script.js
    whitespace adjusting

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -6,13 +6,11 @@
 const loginFormElement = document.querySelector('#loginForm');
 
 
-
 /**
 * Membuat variabel inputEmailElement  untuk tampilan input email.
 * @constant {HTMLElement}
 */
 const inputEmailElement = document.querySelector('#inputEmail');
-
 
 
 /**
@@ -22,13 +20,11 @@ const inputEmailElement = document.querySelector('#inputEmail');
 const inputPasswordElement = document.querySelector('#inputPassword');
 
 
-
 /**
 * Membuat variabel expectedEmail  untuk menyimpan informasi email sementara.
 * @constant {string}
 */
 const expectedEmail = 'admin@dicoding.com';
-
 
 
 /**
@@ -38,42 +34,36 @@ const expectedEmail = 'admin@dicoding.com';
 const expectedPassword = 'superpassword';
 
 
-
 /* Comment : Menanbahkan aksi klik pada button */ 
 loginFormElement.addEventListener('submit', function(event) {
   event.preventDefault();
 
-  
-  
+    
   /**
   * Membuat variabel email  untuk menyimpan nilai email yang didapatkan saat button ditekan.
   * @constant {string}
   */
   const email = inputEmailElement.value;
 
-
+  
   /**
   * Membuat variabel password  untuk menyimpan nilai password yang didapatkan saat button ditekan.
   * @constant {string}
   */
   const password = inputPasswordElement.value;
 
-
+  
   /* Comment : Memastikan bahwa nilai email dan password sesuai dengan nilai yang tersimpan. */ 
   if (email == expectedEmail && password == expectedPassword) {
-
-    
+  
     /* Comment : Jika sesuai maka program akan berpindah ke halaman home. */
     goToHome();
-
     
   } else {
 
-    
     /* Comment : Namun jika tidak maka akan menampilkan informasu bahwa input salah. */
     showPopUp();
 
-    
-    
+        
   }
 });
